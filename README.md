29c9b6575c73a0fc2549702cf58f2dc5589614be
Zandler Oliveira <zandler@outlook.com>
2025-03-27 12:25:38 -0700

    add resources.json

--- README.md
+++ README.md
@@ -3,6 +3,20 @@
 ## A submodule for dotfiles repo (http://github.com/Zandler/dotfiles)
 ### But you can use only this.... 
 
+### Objectives
+Configure my terminal and install some apps that i need in my work. 
+This repos ins`t for everyone, you can use if you want, but first, read the script.
+
+
+### Why Scoop?
+
+Scoop act like apt with some security precautions:
+    - You dont need admin rigths
+    - scoop creates a scoop folde rin your $HOME ans install all apps inside
+    - Scoop add in PATH to the folder and *voil'a* 
+
+More security and agility, less complexity and problem with SecIT
+
 ### Requirements
 
 - Windows 11 (recomended a clean environment)
@@ -12,13 +26,17 @@
 
 1. Update yout system
 2. Install dependencies 
-3. Run this command  in powershell **(No admin local need.)**
+3. Run this two commands  in powershell **(No admin local need.)**
 
 ```powershell
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
 Iwr -Uri https://raw.githubusercontent.com/Zandler/dotfiles-windows/refs/heads/main/install.ps1 -OutFile install.ps1; ./install.ps1
 
 ```
+
+Set-executionPolicy allow you execute powershell script **ONLY** in your current user.
 
 This script:
 - Install scoop
@@ -28,5 +46,12 @@ This script:
 - Config terminal
 - Install WSL , if you are admin local. If not, skip this step.
 
-**If you want install more softwares, go to https://scoop.sh/#/apps and search you app. Next add after line 70**
+## Add more app and buckets
+
+**Buckets** = are like apt repositories. You can found some apps inside.
+**Apps** = applications for you needs. 
+
+**If you want install more softwares, go to https://scoop.sh/#/apps and search you app. 
+
+After found , edit resources.json file add inside buckets or apps. The script will read this file
 
--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 # Requirements
 <#
     .SYNOPSIS
-    Powershell script for install and configure environemnt.
+    Powershell script for install and configure sccop and git
     
     .DESCRIPTION
     Requirements: 
@@ -11,66 +11,85 @@
         - Install scoop
         - install git
         - clone repo
-        - execute bootstrap.ps1
+        - execute sertup.Ps1
 #>
 
 $ErrorActionPreference = 'SilentlyContinue' 
 
-function InstallScoop {
+function InstallScoop 
+{
     try {
-        Clear-Host
+
+        
         Write-Host "Check Scoop is present" -ForegroundColor DarkCyan
         Start-Sleep -Seconds 2
         
         scoop --version
 
-        Write-Host "Scoop found." -ForegroundColor DarkCyan
+        Write-Host "Scoop found." -ForegroundColor Green
         Start-Sleep -Seconds 2
+
     }
-    catch {
-        Clear-Host
-        Write-Host "Scoop not found. Installing ..." -ForegroundColor DarkBlue
+    catch 
+    {
+
+        Write-Host "Scoop not found. Installing ..." -ForegroundColor DarkRed
         Start-Sleep -Seconds 2 
         
         Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
         Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+
+        Write-Host "Scoop Installed. Enjoy." -ForegroundColor DarkGreen
+        Start-Sleep -Seconds 3
     }
 }
+
+function InstallGit 
+{
+    Write-Host "Installing Git with scoop..." -ForegroundColor DarkCyan
+    Start-Sleep -Seconds 3
+
+    scoop install main/git
+}
+
 
 function CloneRepo {
 
     
-    Write-Host "Check if .dotfiles exists in '$HOME' " -ForegroundColor DarkCyan
+    Write-Host "Check if .dotfiles folder exists in '$HOME' " -ForegroundColor DarkCyan
+    Start-Sleep -Seconds 2
     if (Test-Path $HOME\.dotfiles) {
-        Write-Host "folder found. Update..." -ForegroundColor DarkGreen
+        Write-Host "folder found. Update..." -ForegroundColor Green
         cd $HOME\.dotfiles ; git pull
         Start-Sleep -Seconds 2
         Clear-Host
     }
-
-    Write-Host "Clone config files at $HOME\.dotfiles ..."
+    Write-Host "Folder not found. Creating folder." -ForegroundColor DarkCyan
+    Write-Host "Clone config files at $HOME\.dotfiles ..." -ForegroundColor DarkCyan
     git clone https://github.com/zandler/dotfiles-windows.git $HOME\.dotfiles 
     cd $HOME\.dotfiles ; 
+    Write-Host "Folder created, repo cloned." -ForegroundColor Green
     Start-Sleep -Seconds 2
 }
 
-Clear-Host
 
-write-host "Installing Scoop..." -foregroundcolor darkcyan
-InstallScoop
-Start-Sleep -Seconds 2
 
-Clear-Host
+function Init {
+    Clear-Host
+    Write-Host " 
+    =========================================
+    = Config Windows Environment            =
+    = Author: Zandler <zandler@outlook.com> =
+    = Version: 0.9                          =
+    = github.com/zandler/dotfiles-windows   =
+    ========================================= " -ForegroundColor Green
+    Start-Sleep -Seconds 3
 
-Write-Host "Installing Git" 
-scoop install main/git
-Start-Sleep -Seconds2
+}
 
 Clear-Host 
 
+Init
+InstallScoop
+InstallGit
 CloneRepo
-
-Write-Host "Now, starting all conf" -ForegroundColor DarkCyan
-Set-Location -Path $HOME\.dotfiles       
-.\bootstrap.ps1
-
